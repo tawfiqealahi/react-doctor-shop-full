f925a62af589f2e16c5d42732f0d1b453e71233f
tawfiqealahi <tawfiqealahi99@gmail.com>
2021-10-14 23:36:43 -0500

    second commit

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+    return (
+        <div className="service">
+            <h3 className="threed" >  PREVENTION</h3>
+            <h2 className="prevent-heading">How to Prevent Yourself</h2>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing <br /> elit. Aenean
+ligula eget dolor. Aenean massa.</p>
+        </div>
+    );
+};
+
+export default Services;
